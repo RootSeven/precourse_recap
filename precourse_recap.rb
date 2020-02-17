0f0e1ffe90d18d76ff6d94cf7480310a5b647de8
RootSeven <mairifairbairn@gmail.com>
2020-02-17 16:03:57 +0000

    wk1d1 homework: hash, array, loop, function, conditional

---
+++ precourse_recap.rb
@@ -0,0 +1,25 @@
+# Function
+def print_text
+  puts "Text"
+end
+
+# Conditional statement
+if 1 > 2
+  puts "True"
+else
+  puts "False"
+end
+
+# Array
+list = ["Apple", "Orange", "Banana"]
+
+# Hash
+my_hash = {"first_name" => "Mairi",
+  "surname" => "Fairbairn",
+  "age" => "22"
+}
+
+# Loop
+list.each do |item|
+  puts item
+end
